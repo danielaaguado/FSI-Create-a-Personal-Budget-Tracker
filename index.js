13951dc0324a1fe5394552096dd91b77993c6933
danielaaguado <daniela.aguado001@gmail.com>
2022-04-22 08:53:03 -0400

    Added weekly questions

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+let answer = 0
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
